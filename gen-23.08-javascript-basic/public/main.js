6109c4992d6572bc07312a81fc5b917633732ee1
dandi200399 <dandifernando31@gmail.com>
2023-10-03 11:08:54 +0700

    update

---
+++ gen-23.08-javascript-basic/public/main.js
@@ -0,0 +1,45 @@
+
+// Function untuk kotak merah
+function onClickKotakMerah() {
+    console.log('Kotak Merah Diklik');
+}
+
+// Function untuk kotak putih
+function onClickKotakPutih() {
+    console.log('Kotak Putih Diklik');
+}
+
+// Function untuk kotak hitam
+function onClickKotakHitam() {
+    console.log('Kotak Hitam Diklik');
+}
+
+// Function untuk kotak S
+function onClickKotakS() {
+    console.log('Kotak S Diklik');
+}
+
+// Function untuk kotak M
+function onClickKotakM() {
+    console.log('Kotak M Diklik');
+}
+
+// Function untuk kotak L
+function onClickKotakL() {
+    console.log('Kotak L Diklik');
+}
+
+// Function untuk kotak XL
+function onClickKotakXL() {
+    console.log('Kotak XL Diklik');
+}
+
+// Function untuk kotak Go to cart
+function onClickKotakCart() {
+    console.log('Kotak Go to cart Diklik');
+}
+
+// Function untuk kotak Save
+function onClickKotakSave() {
+    console.log('Kotak Save Diklik');
+}
